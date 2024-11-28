eb84890cf340c70ccae6f532b33ce03ae0c92363
lyeong1010 <lyeongeun1010@naver.com>
2024-11-29 01:03:06 +0900

    Update conf.py with required settings

--- docs/conf.py
+++ docs/conf.py
@@ -14,12 +14,21 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+import os
+import sys
+
+# 프로젝트 경로 추가
+sys.path.insert(0, os.path.abspath('../'))  # 루트 디렉토리로 경로 추가
+
+# 확장 기능 설정
+extensions = [
+    'sphinx.ext.autodoc',       # 자동 문서화를 위한 확장
+    'sphinx.ext.viewcode',      # 소스 코드 보기 확장
+    'sphinx.ext.napoleon',      # Google 및 NumPy 스타일 docstring 지원
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
